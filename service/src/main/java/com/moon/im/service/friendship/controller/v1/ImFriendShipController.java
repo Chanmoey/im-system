1f77e1ea0a1aab64b58cb2606e9726af643a7a26
Chanmoey <relijifa@gmail.com>
2023-02-03 16:42:43 +0800

    改造好友关系的controller层，减少多余的return，有非正常的分支直接抛异常，让全局处理器处理。

--- service/src/main/java/com/moon/im/service/friendship/controller/v1/ImFriendShipController.java
+++ service/src/main/java/com/moon/im/service/friendship/controller/v1/ImFriendShipController.java
@@ -1,6 +1,7 @@
 package com.moon.im.service.friendship.controller.v1;
 
 import com.moon.im.common.ResponseVO;
+import com.moon.im.service.friendship.dao.ImFriendShipEntity;
 import com.moon.im.service.friendship.model.req.*;
 import com.moon.im.service.friendship.model.resp.CheckFriendShipResp;
 import com.moon.im.service.friendship.model.resp.ImportFriendShipResp;
@@ -24,57 +25,63 @@ public class ImFriendShipController {
 
     @PostMapping("/importFriendShip")
     public ResponseVO<ImportFriendShipResp> importFriendShip(@RequestBody @Validated ImportFriendShipReq req) {
-        return friendService.importFriendShip(req);
+        return ResponseVO.successResponse(friendService.importFriendShip(req));
     }
 
     @PostMapping("/addFriend")
     public ResponseVO<Object> addFriend(@RequestBody @Validated AddFriendReq req) {
-        return friendService.addFriend(req);
+        friendService.addFriend(req);
+        return ResponseVO.successResponse();
     }
 
     @PostMapping("/updateFriend")
     public ResponseVO<Object> addFriend(@RequestBody @Validated UpdateFriendReq req) {
-        return friendService.updateFriend(req);
+        friendService.updateFriend(req);
+        return ResponseVO.successResponse();
     }
 
     @DeleteMapping("/deleteFriend")
     public ResponseVO<Object> deleteFriend(@RequestBody @Validated DeleteFriendReq req) {
-        return friendService.deleteFriend(req);
+        friendService.deleteFriend(req);
+        return ResponseVO.successResponse();
     }
 
     @DeleteMapping("/deleteAllFriend")
     public ResponseVO<Object> deleteAllFriend(@RequestBody @Validated DeleteFriendReq req, Integer appId) {
         req.setAppId(appId);
-        return friendService.deleteAllFriend(req);
+        friendService.deleteAllFriend(req);
+        return ResponseVO.successResponse();
     }
 
     @PostMapping("/getAllFriend")
-    public ResponseVO<Object> getAllFriend(@RequestBody @Validated GetAllFriendShipReq req) {
-        return friendService.getAllFriendShip(req);
+    public ResponseVO<List<ImFriendShipEntity>> getAllFriend(@RequestBody @Validated GetAllFriendShipReq req) {
+        return ResponseVO.successResponse(friendService.getAllFriendShip(req));
     }
 
     @GetMapping("/getRelation")
-    public ResponseVO<Object> getRelation(@RequestBody @Validated GetRelationReq req) {
-        return friendService.getRelation(req);
+    public ResponseVO<ImFriendShipEntity> getRelation(@RequestBody @Validated GetRelationReq req) {
+        return ResponseVO.successResponse(friendService.getRelation(req));
     }
 
     @PostMapping("/checkFriendship")
     public ResponseVO<List<CheckFriendShipResp>> checkFriendship(@RequestBody @Validated CheckFriendShipReq req) {
-        return friendService.checkFriendship(req);
+        return ResponseVO.successResponse(friendService.checkFriendship(req));
     }
 
     @PostMapping("/addBlack")
     public ResponseVO<Object> addBlack(@RequestBody @Validated AddFriendShipBlackReq req) {
-        return friendService.addBlack(req);
+        friendService.addBlack(req);
+        return ResponseVO.successResponse();
     }
 
     @DeleteMapping("/deleteBlack")
     public ResponseVO<Object> deleteBlack(@RequestBody @Validated DeleteBlackReq req) {
-        return friendService.deleteBlack(req);
+        friendService.deleteBlack(req);
+        return ResponseVO.successResponse();
     }
 
     @PostMapping("/checkBlack")
-    public ResponseVO<Object> checkBlack(@RequestBody @Validated CheckFriendShipReq req) {
-        return friendService.checkBlack(req);
+    public ResponseVO<List<CheckFriendShipResp>> checkBlack(@RequestBody @Validated CheckFriendShipReq req) {
+        return ResponseVO.successResponse(friendService.checkBlack(req));
     }
 }
